decc7fc271ad74249388673e04a1a38afeb54ec3
Kyle Kestell <kyle@kestel.org>
2022-04-06 08:58:14 -0500

    test

--- Ara/Program.cs
+++ Ara/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Immutable;
-using System.Text.Json;
 using Antlr4.Runtime;
 using Antlr4.Runtime.Misc;
 using Ara.Ast.Nodes;
@@ -18,37 +17,10 @@ parser.BuildParseTree = true;
 var tree = parser.program();
 var visitor = new ProgramBuilder();
 var prog = visitor.Visit(tree);
-Console.WriteLine(
-    JsonSerializer.Serialize(
-        prog.Functions.First().Block.Statements,
-        new JsonSerializerOptions { WriteIndented = true, MaxDepth = 0 }));
+Console.WriteLine(prog.ToString());
 
 namespace Ara.Ast.Nodes
 {
-    public readonly record struct Name(string Value);
-    public readonly record struct Atom(string Value);
-    public readonly record struct Type(string Value);
-    public readonly record struct Program(IImmutableList<Function> Functions);
-    public readonly record struct Function(Name Name, Type Type, IImmutableList<Parameter> Parameters, Block Block);
-    public readonly record struct Parameter(Name Name, Type Type);
-    public readonly record struct Block(IImmutableList<IStatement> Statements);
-
-    public interface IStatement
-    {
-    }
-
-    public readonly record struct VariableInitializationStatement(Name Name, IExpression Expression) : IStatement;
-    public readonly record struct VariableDeclarationStatement(Name Name, Type Type) : IStatement;
-    public readonly record struct ReturnStatement(IExpression ReturnValue) : IStatement;
-    public readonly record struct IfStatement(IExpression Condition, Block Block) : IStatement;
-    public readonly record struct AssignmentStatement(Name Name, IExpression Expression) : IStatement;
-
-    // AssignmentExpressionStatement, CallExpressionStatement
-
-    public interface IExpression
-    {
-    }
-
     public enum UnaryOp
     {
     }
@@ -57,131 +29,144 @@ namespace Ara.Ast.Nodes
     {
     }
 
-    public readonly record struct ParensExpression(IExpression Expression) : IExpression;
-    public readonly record struct UnaryExpression(UnaryOp Op, IExpression Right) : IExpression;
-    public readonly record struct InfixExpression(InfixOp Op, IExpression Left, IExpression Right) : IExpression;
-    public readonly record struct FunctionCallExpression(Name Name, IImmutableList<IExpression> Arguments) : IExpression;
-    public readonly record struct AtomExpression(Atom Atom) : IExpression;
+    public readonly record struct Location(int Line, int Column);
+
+    public abstract record class AstNode(Location Location);
+
+    public abstract record class Statement(Location Location) : AstNode(Location);
+    public abstract record class Expression(Location Location) : AstNode(Location);
+
+    public record class Name(string Value, Location Location) : AstNode(Location);
+    public record class Atom(string Value, Location Location) : AstNode(Location);
+    public record class Type(string Value, Location Location) : AstNode(Location);
+
+    public record class Program(IImmutableList<Function> Functions, Location Location) : AstNode(Location);
+    public record class Function(Name Name, Type Type, IImmutableList<Parameter> Parameters, Block Block, Location Location) : AstNode(Location);
+    public record class Parameter(Name Name, Type Type, Location Location) : AstNode(Location);
+    public record class Block(IImmutableList<Statement> Statements, Location Location) : AstNode(Location);
+
+    public record class VariableInitializationStatement(Name Name, Expression Expression, Location Location) : Statement(Location);
+    public record class VariableDeclarationStatement(Name Name, Type Type, Location Location) : Statement(Location);
+    public record class ReturnStatement(Expression ReturnValue, Location Location) : Statement(Location);
+    public record class IfStatement(Expression Condition, Block Block, Location Location) : Statement(Location);
+    public record class AssignmentStatement(Name Name, Expression Expression, Location Location) : Statement(Location);
+
+    public record class ParensExpression(Expression Expression, Location Location) : Expression(Location);
+    public record class UnaryExpression(UnaryOp Op, Expression Right, Location Location) : Expression(Location);
+    public record class InfixExpression(InfixOp Op, Expression Left, Expression Right, Location Location) : Expression(Location);
+    public record class FunctionCallExpression(Name Name, IImmutableList<Expression> Arguments, Location Location) : Expression(Location);
+    public record class AtomExpression(Atom Atom, Location Location) : Expression(Location);
 }
 
 class ProgramBuilder : AraBaseVisitor<Ara.Ast.Nodes.Program>
 {
     public override Ara.Ast.Nodes.Program VisitProgram([NotNull] AraParser.ProgramContext context)
     {
-        var functionBuilder = new FunctionBuilder();
-        var functions = context.function()
-            .Select(x => functionBuilder.Visit(x))
-            .ToImmutableList();
-
-        return new Ara.Ast.Nodes.Program(functions);
+        return NodeBuilder.Program(context);
     }
 }
 
-class FunctionBuilder : AraBaseVisitor<Function>
+static class NodeBuilder
 {
-    public override Function VisitFunction([NotNull] AraParser.FunctionContext context)
-    {
-        var id = context.name().value.Text;
+    private static readonly StatementBuilder statementBuilder = new();
+    private static readonly ExpressionBuilder expressionBuilder = new();
 
-        var parameters = context._parameters
-            .Select(x => new Parameter(
-                new Name(x.name().GetText()),
-                new Ara.Ast.Nodes.Type(x.type().GetText())))
-            .ToImmutableList();
+    public static Ara.Ast.Nodes.Program Program(AraParser.ProgramContext context) =>
+        new Ara.Ast.Nodes.Program(Functions(context._functions), Location(context));
 
-        var block = new BlockBuilder().Visit(context.block());
+    public static Block Block(AraParser.BlockContext context) =>
+        new(Statements(context._statements), Location(context));
 
-        return new Function(
-            new Name(context.name().GetText()),
-            new Ara.Ast.Nodes.Type(context.type().GetText()),
-            parameters,
-            block);
-    }
+    public static IImmutableList<Function> Functions(IList<AraParser.FunctionContext> context) =>
+        context.Select(x => new Function(Name(x.name()), Type(x.type()), Parameters(x._parameters), Block(x.block()), Location(x))).ToImmutableList();
+
+    public static VariableInitializationStatement VariableInitializationStatement(AraParser.VariableInitializationStatementContext context) =>
+        new(Name(context.name()), Expression(context.expression()), Location(context));
+
+    public static VariableDeclarationStatement VariableDeclarationStatement(AraParser.VariableDeclarationStatementContext context) =>
+        new(Name(context.name()), Type(context.type()), Location(context));
+
+    public static ReturnStatement ReturnStatement(AraParser.ReturnStatementContext context) =>
+        new(Expression(context.expression()), Location(context));
+
+    public static IfStatement IfStatement(AraParser.IfStatementContext context) =>
+        new(Expression(context.expression()), Block(context.block()), Location(context));
+
+    public static AssignmentStatement AssignmentStatement(AraParser.AssignmentStatementContext context) =>
+        new(Name(context.name()), expressionBuilder.Visit(context.expression()), Location(context));
+
+    public static ParensExpression ParensExpression(AraParser.ParensExpressionContext context) =>
+        new(Expression(context.expression()), Location(context));
+
+    public static UnaryExpression UnaryExpression(AraParser.UnaryExpressionContext context) =>
+        new((UnaryOp)Enum.Parse(typeof(UnaryOp), context.op.Text), Expression(context.expression()), Location(context));
+
+    public static InfixExpression InfixExpression(AraParser.InfixExpressionContext context) =>
+        new((InfixOp)Enum.Parse(typeof(InfixOp), context.op.Text), Expression(context.left), Expression(context.right), Location(context));
+
+    public static FunctionCallExpression FunctionCallExpression(AraParser.FunctionCallExpressionContext context) =>
+        new(Name(context.name()), Arguments(context._arguments), Location(context));
+
+    public static AtomExpression AtomExpression(AraParser.AtomExpressionContext context) =>
+        new(Atom(context.atom()), Location(context));
+
+    private static Atom Atom(AraParser.AtomContext context) =>
+        new(context.GetText(), Location(context));
+
+    private static Name Name(AraParser.NameContext context) =>
+        new(context.GetText(), Location(context));
+
+    private static Ara.Ast.Nodes.Type Type(AraParser.TypeContext context) =>
+        new(context.GetText(), Location(context));
+
+    private static IImmutableList<Expression> Arguments(IList<AraParser.ExpressionContext> context) =>
+        context.Select(x => Expression(x)).ToImmutableList();
+
+    private static IImmutableList<Parameter> Parameters(IList<AraParser.ParameterContext> context) =>
+        context.Select(x => new Parameter(Name(x.name()), Type(x.type()), Location(x))).ToImmutableList();
+
+    private static IImmutableList<Statement> Statements(IList<AraParser.StatementContext> context) =>
+        context.Select(x => statementBuilder.Visit(x)).ToImmutableList();
+
+    private static Location Location(ParserRuleContext context) =>
+        new(context.Start.Line, context.Start.Column);
+
+    private static Expression Expression(AraParser.ExpressionContext context) =>
+        expressionBuilder.Visit(context);
 }
 
-class BlockBuilder : AraBaseVisitor<Block>
+class StatementBuilder : AraBaseVisitor<Statement>
 {
-    public override Block VisitBlock([NotNull] AraParser.BlockContext context)
-    {
-        var statementBuilder = new StatementBuilder();
-        var statements = context._statements
-            .Select(x => statementBuilder.Visit(x))
-            .ToImmutableList();
+    public override Statement VisitVariableInitializationStatement([NotNull] AraParser.VariableInitializationStatementContext context) =>
+        NodeBuilder.VariableInitializationStatement(context);
 
-        return new Block(statements);
-    }
+    public override Statement VisitVariableDeclarationStatement([NotNull] AraParser.VariableDeclarationStatementContext context) =>
+        NodeBuilder.VariableDeclarationStatement(context);
+
+    public override Statement VisitReturnStatement([NotNull] AraParser.ReturnStatementContext context) =>
+        NodeBuilder.ReturnStatement(context);
+
+    public override Statement VisitIfStatement([NotNull] AraParser.IfStatementContext context) =>
+        NodeBuilder.IfStatement(context);
+
+    public override Statement VisitAssignmentStatement([NotNull] AraParser.AssignmentStatementContext context) =>
+        NodeBuilder.AssignmentStatement(context);
 }
 
-class StatementBuilder : AraBaseVisitor<IStatement>
+class ExpressionBuilder : AraBaseVisitor<Expression>
 {
-    private readonly ExpressionBuilder expressionBuilder = new();
-    private readonly BlockBuilder blockBuilder = new();
+    public override Expression VisitParensExpression([NotNull] AraParser.ParensExpressionContext context) =>
+        NodeBuilder.ParensExpression(context);
 
-    public override IStatement VisitVariableInitializationStatement([NotNull] AraParser.VariableInitializationStatementContext context)
-    {
-        return new VariableInitializationStatement(
-            new Name(context.name().GetText()),
-            expressionBuilder.Visit(context.expression()));
-    }
+    public override Expression VisitUnaryExpression([NotNull] AraParser.UnaryExpressionContext context) =>
+        NodeBuilder.UnaryExpression(context);
 
-    public override IStatement VisitVariableDeclarationStatement([NotNull] AraParser.VariableDeclarationStatementContext context)
-    {
-        return new VariableDeclarationStatement(
-            new Name(context.name().GetText()),
-            new Ara.Ast.Nodes.Type(context.type().GetText()));
-    }
+    public override Expression VisitInfixExpression([NotNull] AraParser.InfixExpressionContext context) =>
+        NodeBuilder.InfixExpression(context);
 
-    public override IStatement VisitReturnStatement([NotNull] AraParser.ReturnStatementContext context)
-    {
-        return new ReturnStatement(expressionBuilder.Visit(context.expression()));
-    }
+    public override Expression VisitFunctionCallExpression([NotNull] AraParser.FunctionCallExpressionContext context) =>
+        NodeBuilder.FunctionCallExpression(context);
 
-    public override IStatement VisitIfStatement([NotNull] AraParser.IfStatementContext context)
-    {
-        return new IfStatement(
-            expressionBuilder.Visit(context.expression()),
-            blockBuilder.Visit(context.block()));
-    }
-
-    public override IStatement VisitAssignmentStatement([NotNull] AraParser.AssignmentStatementContext context)
-    {
-        return new AssignmentStatement(
-            new Name(context.name().GetText()),
-            expressionBuilder.Visit(context.expression()));
-    }
-}
-
-class ExpressionBuilder : AraBaseVisitor<IExpression>
-{
-    public override IExpression VisitParensExpression([NotNull] AraParser.ParensExpressionContext context)
-    {
-        return new ParensExpression(Visit(context.expression()));
-    }
-
-    public override IExpression VisitUnaryExpression([NotNull] AraParser.UnaryExpressionContext context)
-    {
-        return new UnaryExpression(
-            (UnaryOp)Enum.Parse(typeof(UnaryOp), context.op.Text),
-            Visit(context.expression()));
-    }
-
-    public override IExpression VisitInfixExpression([NotNull] AraParser.InfixExpressionContext context)
-    {
-        return new InfixExpression(
-            (InfixOp)Enum.Parse(typeof(InfixOp), context.op.Text),
-            Visit(context.left),
-            Visit(context.right));
-    }
-
-    public override IExpression VisitFunctionCallExpression([NotNull] AraParser.FunctionCallExpressionContext context)
-    {
-        return new FunctionCallExpression(
-            new Name(context.name().GetText()),
-            context._arguments.Select(x => Visit(x)).ToImmutableList());
-    }
-
-    public override IExpression VisitAtomExpression([NotNull] AraParser.AtomExpressionContext context)
-    {
-        return new AtomExpression(new Atom(context.GetText()));
-    }
+    public override Expression VisitAtomExpression([NotNull] AraParser.AtomExpressionContext context) =>
+        NodeBuilder.AtomExpression(context);
 }
